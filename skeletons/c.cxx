374758754b23115e9ea554be624b6ed1f0a469b7
Jason Allen <jallen02@fxdeva02.factset.com>
2016-08-01 12:49:53 -0400

    added skeletons and tab behavior for vim

---
+++ skeletons/c.cxx
@@ -0,0 +1,4 @@
+
+// --------
+#include "mem_manager.h"    // must be the final #include header
+// --------
---
+++ skeletons/c.h
@@ -0,0 +1,8 @@
+#pragma once  /* makefds directive - do not remove this line */
+
+#ifndef _H_YOUR_FILE_NAME_HERE
+#define _H_YOUR_FILE_NAME_HERE
+
+
+
+#endif /* _H_YOUR_FILE_NAME_HERE */
